938eea9d64572c30d2ec1d356b4a553ec68b6f39
dmtuong <dmtuong@cmc.com.vn>
2019-10-29 23:02:47 +0700

    demo

---
+++ src/components/childComponent/componentChild.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+    <div class="card m-3" style="width: 25rem;">
+      <div class="card-body">
+        <h5 class="card-title" v-text="thecardtitle"></h5>
+        <p class="card-text" v-html="thecardbody"></p>
+        <div v-if="parentmessage" class="card-text alert alert-warning" v-html="parentmessage"></div>
+        <button v-if="parentmessage" @click="onClick" class="btn btn-success">Ok</button>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  props: ["parentmessage"],
+  data() {
+    return {
+      thecardtitle: "Child Component!",
+      thecardbody: "I'm just a child."
+    };
+  },
+  methods: {
+    onClick() {
+      this.$emit("finished");
+    }
+  }
+};
+</script>
+ 
---
+++ src/components/componentParent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="card m-2" style="width: 40rem;">
+      <div class="card-body">
+        <h5 class="card-title" v-text="thecardtitle"></h5>
+        <button @click="sendMessage" class="btn btn-info">Send Child A Message</button>
+        <componentChild :parentmessage="parentmessage" @finished="finished"></componentChild>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import componentChild from "./childComponent/componentChild.vue";
+export default {
+  components: {
+    componentChild
+  },
+  data() {
+    return {
+      thecardtitle: "Parent Component!",
+      parentmessage: ""
+    };
+  },
+
+  methods: {
+    sendMessage() {
+      this.parentmessage = "<b>Message From Parent:</b> Do Your Homework";
+    },
+
+    finished(){
+        this.parentmessage = '';
+    }
+  }
+};
+</script>
